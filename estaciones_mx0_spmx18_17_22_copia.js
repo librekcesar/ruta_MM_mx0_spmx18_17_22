96672a95092909531ea0a979f5302728a198c3f5
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 17:32:43 -0600

    Add files via upload

---
+++ estaciones_mx0_spmx18_17_22_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_spmx18_17_22",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx.", "description": "MX0", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1476988, 19.6397536, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Isla Terranova 747, Villa Esmeralda, 54910 Fuentes del Valle, Méx.", "description": "SPMX18", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1293444, 19.6431605, 0.0 ] } }
+]
+}
